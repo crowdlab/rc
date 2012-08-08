e2d687ce7d84d2334fae5996dafed25bfc0b4109
Stanislav Vorobyev <vissi@vissi.su>
2012-08-08 13:36:51 +0400

    git pull all

---
+++ git-pull-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for x in `ls -1` ; do
+    echo $x:
+    [ -d "$x" ] && cd "$x"
+
+    [ -d ".hg" ] && hg pull
+    [ -d "_darcs" ] && darcs pull
+    [ -d ".bzr" ] && bzr pull
+    [ -d ".svn" ] && svn up
+
+    if [ -d ".git" ]; then
+        if [ $(git svn info 2>&1 | grep "Unable to determine upstream SVN information" | wc -l) -eq 1 ]; then
+            git pull
+        else
+            git svn rebase
+        fi  
+    fi  
+
+    [ -d "../$x" ] && cd ..
+done
